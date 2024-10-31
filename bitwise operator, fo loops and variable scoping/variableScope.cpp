031bad3c85842e8860ec6bdc8f7af4d46aa6e69d
th3m4nd4r1n <mdua_be23@thapar.edu>
2024-10-31 19:18:48 +0530

    did just a scoping example because of diwali

---
+++ bitwise operator, fo loops and variable scoping/variableScope.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 4;
+    cout<<a<<endl;
+    if(true){
+        int a = 6;
+        cout<<a<<endl;
+}
+    cout<<a<<endl;
+    return 0;
+}
